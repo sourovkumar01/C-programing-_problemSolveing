94247968912b4ae749a84256d367ad6c4025355e
Sourov kumar <126077980+sourovkumar01@users.noreply.github.com>
2024-04-22 13:22:07 +0600

    Add files via upload

---
+++ piramid partan.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+ int N = 5;
+ 
+ // Outer Loop for number of rows
+ for (int i = 1; i <= N; i++) {
+ 
+ // inner Loop for space printing
+ for (int j = 1; j <= N - i; j++)
+ printf(" ");
+ 
+ // inner Loop for star printing
+ for (int j = 1; j < 2 * i; j++)
+ printf("1");
+ printf("\n");
+ }
+ return 0;
+}
