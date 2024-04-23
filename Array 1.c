42c921ef1163000d27bc4b088696a888f1232595
Sourov kumar <126077980+sourovkumar01@users.noreply.github.com>
2024-04-23 09:55:11 +0600

    Add files via upload

---
+++ Array 1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+int num [100],n,i;
+printf("How many number :");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&num[i]);
+
+}
+int max= num[0];
+for (i=1;i<n;i++){
+        if (max < num[i]){
+
+        max = num[i];
+        }
+
+}
+printf("Maximum = %d",max);
+
+getch();
+}
