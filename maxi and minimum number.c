661486d950874f64c1a0401472fd0e9dc049acf8
Sourov kumar <126077980+sourovkumar01@users.noreply.github.com>
2024-04-28 23:08:41 +0600

    Add files via upload

---
+++ maxi and minimum number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 55, b = 23;
+
+    // return maximum among the two numbers
+    printf("max = %d\n", ((a + b) + abs(a - b)) / 2);
+
+    // return minimum among the two numbers
+    printf("min = %d", ((a + b) - abs(a - b)) / 2);
+
+    return 0;
+}
